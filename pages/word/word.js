dcc297a45575312691e6b03e7463df10ba22bd4e
wangdy <wangdongyang3239@ipalfish.com>
2020-09-08 17:59:49 +0800

    init

---
+++ pages/word/word.js
@@ -0,0 +1,65 @@
+var app = getApp();
+import { send_photo } from "../../utils/util.js";
+
+Page({
+  data: {
+    serverUri:'https://wangdy.xyz',
+    file1: "../images/111.jpeg",
+    file2: "../images/111.jpeg",
+    file3: ""
+  },
+  formSubmit: function (e) {
+    // , this.data.photos2, this.data.photos3
+    var files = [this.data.file1]
+    send_photo(this.data.serverUri, files, function (res) {
+      console.log("res:",res)
+    })
+  },
+  touchphoto: function (e) {
+    var that = this
+    var no = e.currentTarget.id;
+    if (no == "1") {
+      wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var tempFilePaths = res.tempFilePaths
+          that.setData({
+            file1: tempFilePaths
+          })
+          console.log("file1:",that.data.file1)
+        }
+      })
+    } else if (no == "2") {
+      wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var tempFilePaths = res.tempFilePaths
+          that.setData({
+            file2: tempFilePaths
+          })
+          console.log(that.data.file2)
+        }
+      })
+    } else if (no == "3") {
+      wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var tempFilePaths = res.tempFilePaths
+          that.setData({
+            file3: tempFilePaths
+          })
+          console.log(that.data.file3)
+        }
+      })
+    }
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,42 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+function send_photo(uri, data, successfun) {
+  var that = this
+  for (let i = 0; i < data.length; i++) {
+    console.log("data:", data[i])
+    wx.uploadFile({
+      url: uri,
+      filePath: data[i] + "",
+      name: 'file',
+      success: function (res) {
+        successfun(res)
+      },
+      fail: function (res) {
+        console.log("fail:", res)
+      },
+      complete: function (res) {
+        console.log("complete:", res)
+      },
+    })
+  }
+}
+
+const _send_photo = send_photo
+export { _send_photo as send_photo }
+const _formatTime = formatTime
+export { _formatTime as formatTime }
+
+
